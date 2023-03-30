eb455c01576dfb6ffed12f059aa6dd854ed83cfb
Juan <otkclase21@gmail.com>
2023-03-30 09:46:01 +0200

    Creo constructor

--- Git_Entornos/src/ejercicio_GIT/Alumno.java
+++ Git_Entornos/src/ejercicio_GIT/Alumno.java
@@ -4,4 +4,10 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+	Alumno (String a, String b, String c) {
+		nombre = a;
+		apellido = b;
+		dni = c;
+	}
 }
